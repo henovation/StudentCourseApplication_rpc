38b80fa649a8ddb2df2226abc27118cfba94fb92
michael <michaellemin@hotmail.com>
2022-08-26 17:00:31 -0700

    added data model

--- server.py
+++ server.py
@@ -1,27 +1,61 @@
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures.process import _threads_wakeups
+from http.client import REQUEST_URI_TOO_LONG
+import json
+from subprocess import SubprocessError
 from uuid import uuid4
+from google.protobuf.json_format import MessageToJson
 
 import grpc
 from grpc_reflection.v1alpha import reflection
 import logging
-import rides_pb2 as pb
-import rides_pb2_grpc as rpc
+import redis
+import assignments_pb2 as pb
+import assignments_pb2_grpc as rpc
 
-def new_ride_id():
+r = redis.Redis(
+    host='localhost',
+    port=6379)
+
+
+def generate_student_id():
+    return uuid4().int
+
+def new_course_id():
     return uuid4().hex
 
-class Rides(rpc.RidesServicer):
-    def Start(self, request, context):
-        print('ride: %r', request)
 
-        ride_id = new_ride_id()
-        return pb.StartResponse(id=ride_id)
+class Students(rpc.StudentsServicer):
+    def Create(self, request, context):
+        try:
+            new_student_id = generate_student_id()
+            print("Original Proto Request:", request)
+
+            data = request.SerializeToString()
+            print("StringBytes data:", data)
+            r.set(new_student_id, data)
+            success = True
+        except:
+            success = False
+        return pb.CreateStudentResponse(success=success)
+
+    def Update(self, request, context):
+        try:
+            print('/n/n/nUpdating request:', request)
+            print(type(request.student_id))
+            data = request.SerializeToString()
+            success = True
+        except:
+            print('Something wrong when updating')
+            success = False
+        return pb.UpdateStudentResponse(success=success)
+
 
 if __name__ == '__main__':
     import config
 
-    server = grpc.server(ThreadPoolExecutor())
-    rpc.add_RidesServicer_to_server(Rides(), server)
+    server = grpc.server(ThreadPoolExecutor(max_workers=10))
+    rpc.add_StudentsServicer_to_server(Students(), server)
 
     addr = f'[::]:{config.port}'
     server.add_insecure_port(addr)
